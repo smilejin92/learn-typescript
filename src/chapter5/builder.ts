9c217c821382f7411e39519a65f9ee462915a804
smilejin92 <smilejin92@gmail.com>
2021-09-03 21:41:42 +0900

    [feat] Ch5 예제 코드 추가 5

---
+++ src/chapter5/builder.ts
@@ -0,0 +1,32 @@
+class RequestBuilder<T> {
+  private url: string | null = null;
+  private method: 'get' | 'post' | null = null;
+  private data: T | null = null;
+
+  // 빌더의 메소드는 인스턴스(this)를 반환한다.
+  setURL(url: string): this {
+    this.url = url;
+    return this;
+  }
+
+  setMethod(method: 'get' | 'post'): this {
+    this.method = method;
+    return this;
+  }
+
+  setData(data: T): this {
+    this.data = data;
+    return this;
+  }
+
+  // url, method, data를 설정하였는지 검사가 필요
+  send() {
+    // ...
+  }
+}
+
+new RequestBuilder()
+  .setURL('/users')
+  .setMethod('post')
+  .setData({ firstName: 'Jin' })
+  .send();
---
+++ src/chapter5/factory.ts
@@ -0,0 +1,33 @@
+// 타입스크립트는 값과 타입의 네임스페이스를 따로 관리한다.
+type Shoe = {
+  purpose: string;
+};
+
+class BalletFalt implements Shoe {
+  purpose = 'dancing';
+}
+
+class Boot implements Shoe {
+  purpose = 'woodcutting';
+}
+
+class Sneaker implements Shoe {
+  purpose = 'walking';
+}
+
+// 타입스크립트는 값과 타입의 네임스페이스를 따로 관리한다.
+const Shoe = {
+  create(type: 'balletFlat' | 'boot' | 'sneaker'): Shoe {
+    switch (type) {
+      case 'balletFlat':
+        return new BalletFalt();
+      case 'boot':
+        return new Boot();
+      case 'sneaker':
+        return new Sneaker();
+    }
+  },
+};
+
+// 호출자는 팩토리가 특정 인터페이스를 만족하는 클래스를 제공할 것이라는 사실만 알 뿐, 어떤 구체 클래스가 이일을 하는지 알 수 없어야 한다.
+const boot = Shoe.create('boot'); // Shoe
