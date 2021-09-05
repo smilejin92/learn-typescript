afcfb841aa5e9b25c1a3be3da84e9d6f111abde1
smilejin92 <smilejin92@gmail.com>
2021-09-05 23:47:36 +0900

    [feat] Ch6 예제 코드 추가 1

---
+++ src/chapter6/covariant.ts
@@ -0,0 +1,23 @@
+// 서버로부터 받은 기존 사용자
+type ExistingUser = {
+  id: number;
+  name: string;
+};
+
+// 아직 서버에 저장하지 않은 새 사용자
+type NewUser = {
+  name: string;
+};
+
+function deleteUser(user: { id?: number; name: string }) {
+  delete user.id;
+}
+
+const existingUser: ExistingUser = {
+  id: 123456,
+  name: 'Jin',
+};
+
+deleteUser(existingUser);
+
+existingUser.id;
